8f4f75d62be6f24c4bca3177837ac9c45de5f47e
Aporva2001 <85491512+Aporva2001@users.noreply.github.com>
2024-10-19 11:43:12 +0530

    Code before working on Cart Model

---
+++ module 11 upto 166/controllers/shop.js
@@ -0,0 +1,111 @@
+const Product = require('../models/product');
+const Cart = require('../models/cart');
+const { where } = require('sequelize');
+
+exports.getProducts = (req, res, next) => {
+  Product.findAll().then(products => {
+    res.render('shop/product-list', {
+      prods: products,
+      pageTitle: 'All Products',
+      path: '/products'
+    });
+  }).catch(err =>{
+    console.log(err);
+  })
+};
+
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+
+  // Product.findAll({where: {id: prodId}}).then((products)=>{  // With sequelize we dont get an array of products but we get a single product.
+  //   console.log(products);
+  //   res.render('shop/product-detail', {
+  //     product: products[0],
+  //     pageTitle: products[0].title,
+  //     path: '/products'
+  //   });
+  // }).catch(err => console.log(err));  // Unlike findByPk this gives us an array of products
+
+
+  // Another Apporach in place of using findAll....
+
+  Product.findByPk(prodId)
+  .then((product)=>{  // With sequelize we dont get an array of products but we get a single product.
+    console.log(product);
+    res.render('shop/product-detail', {
+      product: product,
+      pageTitle: product.title,
+      path: '/products'
+    });
+  })
+  .catch((err)=>{
+    console.log(err)
+  });
+
+};
+
+exports.getIndex = (req, res, next) => {
+  Product.findAll().then(products => {
+    res.render('shop/index', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/'
+    });
+  }).catch(err =>{
+    console.log(err);
+  })
+  
+};
+
+exports.getCart = (req, res, next) => {
+  Cart.getCart(cart =>{
+    Product.fetchAll(products =>{
+      const cartProducts=[];
+      for(product of products){
+        const cartProductData = cart.products.find(prod => prod.id === product.id)
+
+        if(cartProductData){
+          cartProducts.push({productData: product, qty:cartProductData.qty})
+        }
+      }
+      res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your Cart',
+        products:cartProducts
+      });
+    })
+  })
+
+};
+
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findByPk(prodId, product => {
+    Cart.addProduct(prodId, product.price);
+  });
+  res.redirect('/cart');
+};
+
+exports.getOrders = (req, res, next) => {
+  res.render('shop/orders', {
+    path: '/orders',
+    pageTitle: 'Your Orders'
+  });
+};
+
+exports.getCheckout = (req, res, next) => {
+  res.render('shop/checkout', {
+    path: '/checkout',
+    pageTitle: 'Checkout'
+  });
+};
+
+exports.postCartDeleteProduct = (req,res, next)=>{
+  const prodId= req.body.productId;
+  Product.findById(prodId, product =>{
+    Cart.deleteProduct(prodId, product.price);
+    res.redirect('/cart');
+
+  })
+
+}
