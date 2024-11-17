71ba2f4224b7d9590e1f2660f3813e088e3d28ba
Aporva2001 <85491512+Aporva2001@users.noreply.github.com>
2024-11-17 19:08:26 +0530

    Module 16 Completed
    Fixed an issue with sendGrid

---
+++ module 16 completed/routes/admin.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+const express = require('express');
+
+const adminController = require('../controllers/admin');
+
+const isAuth= require('../middleware/is-auth');
+
+const router = express.Router();
+
+// /admin/add-product => GET
+router.get('/add-product', isAuth, adminController.getAddProduct); // The requests are parsed from left to right, so we have added a middleware after the route.
+
+// /admin/products => GET
+router.get('/products', isAuth, adminController.getProducts);
+
+// /admin/add-product => POST
+router.post('/add-product', isAuth, adminController.postAddProduct);
+
+router.get('/edit-product/:productId', isAuth, adminController.getEditProduct);
+
+router.post('/edit-product', isAuth, adminController.postEditProduct);
+
+router.post('/delete-product', isAuth, adminController.postDeleteProduct);
+
+module.exports = router;
